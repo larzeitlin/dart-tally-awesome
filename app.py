eb7b6f6c6bb72f7d62e8849f445a6262821bb21a
luke-zeitlin <luke.zeitlin@profinda.com>
2018-11-24 12:46:40 +0000

    Demo

--- app.py
+++ app.py
@@ -21,7 +21,26 @@ app = dash.Dash(__name__, server=server)
 
 
 # Put your Dash code here
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
 
 # Run the Dash app
 if __name__ == '__main__':
