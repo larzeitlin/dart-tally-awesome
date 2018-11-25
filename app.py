a12a700ac6421f497679999bbc83509b86b7506f
luke-zeitlin <luke.zeitlin@profinda.com>
2018-11-25 17:22:34 +0000

    UI IMPROVEMENTS

--- app.py
+++ app.py
@@ -1,53 +1,148 @@
-# Import required libraries
 temp_db_link = "postgres://mzdscoykshzgbv:189b1d7b220b1c4c22fbee4fabb30042e5cca2be6abb00a526e568ca9098c8b8@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dcmbq4go3nl1bs"
 
 import os
 from random import randint
-
 import plotly.plotly as py
 from plotly.graph_objs import *
-
 import flask
 import dash
+import dash_table
 from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
+import sqlalchemy
+import pandas as pd
+from datetime import datetime
 
-
-# Setup the app
-# Make sure not to change this file name or the variable names below,
-# the template is configured to execute 'server' on 'app.py'
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, server=server)
-
-
-# Put your Dash code here
-
-import sqlalchemy
-import pandas as pd
 #conn = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
-#   conn = sqlalchemy.create_engine(temp_db_link)
-#   x = conn.execute('SELECT * FROM player_ids')
-
-#   conn.execute("DELETE FROM player_ids WHERE player_ids.player_id=1;" )
-#   conn.execute("INSERT INTO player_ids (player_name) VALUES('test2');")
-#   df = pd.read_sql(sql='SELECT * FROM player_ids;', 
-#                    con=conn)
-#   print(df)
-
+conn = sqlalchemy.create_engine(temp_db_link)
+init_player_df = pd.read_sql(sql='SELECT * FROM player_ids;', con=conn)
+init_game_log_df = pd.read_sql(sql='SELECT * FROM game_log;', con=conn, parse_dates=['created_at'])
+#init_game_log_df['created_at'] = init_game_log_df['created_at'].dt.strftime("%D, %r")
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-    dcc.Input(
-        placeholder='Enter a value...',
-        type='text',
-        value='')
+    html.H1(children="Dart-Tally-Awesome",
+            style={'textAlign' : 'center'}),
+    html.Div([
+        html.Div([html.Div("Player 1"),
+                  dcc.Dropdown(options=[{'label' : i['player_name'],
+                                         'value' : i['player_id']} 
+                                        for i in init_player_df.to_dict("rows")],
+                               id="player_2_dropdown"),
+                  html.Div("Player 1 Score"),
+                  html.Div(dcc.Input(id='player_1_score',
+                                     value='', 
+                                     type='text'))], 
+                className="four columns",
+                style={'textAlign' : 'center'}),
+        html.H3("-VS-",
+                className="four columns"),
+
+        html.Div([html.Div("Player 2"),
+                  dcc.Dropdown(options=[{'label' : i['player_name'],
+                                         'value' : i['player_id']} 
+                                        for i in init_player_df.to_dict("rows")],
+                               id="player_1_dropdown"),
+                  html.Div("Player 2 Score"),
+                  html.Div(dcc.Input(id='player_2_score',
+                                     value='', 
+                                     type='text'))], 
+                className="four columns",
+                style={'textAlign' : 'center'}), 
+        html.Button("Add Score",
+                    id="add_score_button",
+                    style={"textAlign" : 'center'})],
+        className="row",
+        style={"textAlign" : 'center'}),
+    html.Div(children=[
+        html.H4("Add a player"),
+        html.Div(dcc.Input(id='new_player_input_field', 
+                           value='', 
+                           type='text'),
+                           style={"textAlign" : "center"}),
+        html.Div([
+            html.Button("Submit", 
+                        id='add_new_player_button')])],
+        style={"textAlign" : "center"}),
+    html.Div(children=[
+        html.H4("League Table"),
+        dash_table.DataTable(id='player_table',
+            columns=[{"name" : i, "id" : i} for i in init_player_df.columns],
+            data=init_player_df.to_dict("rows"),
+            style_cell={'textAlign' : 'center'})],
+            style={'textAlign' : 'center'},
+            className="twelve columns"),
+
+    html.Div(children=[
+        html.H4("Game Table"),
+        dash_table.DataTable(id='game_table',
+            columns=[{"name" : i, "id" : i} for i in init_game_log_df.columns],
+            data=init_game_log_df.to_dict("rows"),
+            style_cell={'textAlign' : 'center'})],
+            style={'textAlign' : 'center'},
+            className="twelve columns"),
 ])
 
-# Run the Dash app
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+@app.callback(
+    Output(component_id='player_table', component_property='data'),
+    [Input('add_new_player_button', 'n_clicks')],
+    [State('new_player_input_field', 'value')])
+def update_league_table(n_clicks, value):
+    init_df = pd.read_sql(sql='SELECT * FROM player_ids;', con=conn)
+    if value not in ["", None, "enter a new player"] + init_df['player_name'].tolist():
+        conn.execute("INSERT INTO player_ids (player_name) VALUES('{}');".format(value))
+    df = pd.read_sql(sql='SELECT * FROM player_ids;', con=conn)
+    return(df.to_dict("rows"))
+
+@app.callback(
+    Output(component_id='game_table', component_property='data'),
+    [Input('add_score_button', 'n_clicks_timestamp')],
+    [State('player_1_dropdown', 'value'),
+    State('player_2_dropdown', 'value'),
+    State('player_1_score', 'value'),
+    State('player_2_score', 'value')])
+def update_games(timestamp, p1_id, p2_id, p1_score, p2_score):
+    df = pd.read_sql(sql='SELECT * FROM game_log;', con=conn, parse_dates=['created_at'])
+ #  df['created_at'] = df['created_at'].dt.strftime("%D, %r")
+    print("p1_id: ", p1_id)
+    print("p1_score: ", p1_score)
+    try:
+        p1_id = int(p1_id)
+        p2_id = int(p2_id)
+        p1_score = int(p1_score)
+        p2_score = int(p2_score)
+    except TypeError:
+        print("Here")
+        return(df.to_dict("rows"))
+    if p1_score >= p2_score:
+        winner_id = p1_id
+        winner_score = p1_score
+        looser_id = p2_id
+        looser_score=p2_score
+    else:
+        winner_id = p2_id
+        winner_score = p2_score
+        looser_id = p1_id
+        looser_score=p1_score
+    utc_stamp = str(datetime.utcfromtimestamp(int(timestamp / 1000)))
+
+    conn.execute("INSERT INTO game_log "\
+    "(winner_id, winner_score, looser_id, looser_score, created_at) "\
+    "VALUES({}, {}, {}, {}, '{}');".format(winner_id,
+                                                      winner_score,
+                                                      looser_id,
+                                                      looser_score,
+                                                      utc_stamp))
+
+    df = pd.read_sql(sql='SELECT * FROM game_log;', con=conn, parse_dates=['created_at'])
+    return(df.to_dict("rows"))
+
 if __name__ == '__main__':
     app.server.run(debug=True, threaded=True)
---
+++ elo_score.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import sqlalchemy
+from datetime import datetime, timedelta
+import math
+
+def expected_score(RA, RB):
+    EA = 1 / (1 +  (math.pow(10, ((RB - RA)/400))))
+    return(EA)
+
+def calculate_new_elo_scores(elo_scores, day_games, K=20):
+    for index, row in day_games.iterrows():
+        winner_exp = expected_score(elo_scores[row["winner_id"]], row['looser_id'])
+        looser_exp = 1 - winner_exp
+        winner_new_elo = elo_scores[row['winner_id']] + K * (row['winner_score'] - winner_exp)
+        looser_new_elo = elo_scores[row['looser_id']] + K * (row['looser_score'] - looser_exp)
+        elo_scores[row['winner_id']] = winner_new_elo
+        elo_scores[row['looser_id']] = looser_new_elo
+    return(elo_scores)
+
+def make_elo_scores_df(game_log_df, player_ids_df, K=20):
+    start_date = min(game_log_df['created_at'] - timedelta(days=1)).date()
+    end_date = max(game_log_df['created_at'] + timedelta(days=1)).date()
+    date_range = [start_date + timedelta(days=x) for x in range(0, (end_date - start_date).days)]
+    player_ids = player_ids_df['player_id'].tolist()
+    out_df = pd.DataFrame(index=date_range, columns=player_ids)
+    elo_scores = {i : 100 for i in player_ids}
+    for index, row in out_df.iterrows():
+        day_games = game_log_df[game_log_df['created_at'].dt.date == index]
+        if day_games.shape[0] == 0:
+            out_df.at[index] = elo_scores
+        else:
+            elo_scores = calculate_new_elo_scores(elo_scores, day_games, K)
+            out_df.at[index] = elo_scores
+    print(out_df)
